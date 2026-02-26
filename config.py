de3687173336de70d3bcd3491afd3dc763245779
safeer <abc@gmail.com>
2026-02-26 19:52:30 +0530

    updated

--- config.py
+++ config.py
@@ -1,7 +1,8 @@
 import streamlit as st
 
 GROQ_API_KEY = st.secrets["GROQ_API_KEY"]
-MONDAY_API_KEY =st.secrets["MONDAY_API_TOKEN"]
+MONDAY_API_KEY = st.secrets["MONDAY_API_TOKEN"]
 
 DEALS_BOARD_ID = st.secrets["DEALS_BOARD_ID"]
 WORK_ORDERS_BOARD_ID = st.secrets["WORK_ORDERS_BOARD_ID"]
+
